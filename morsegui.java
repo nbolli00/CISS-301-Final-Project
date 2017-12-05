5cadb6fa9d2358f9717453ca6719aae326e56856
nbolli00 <31674741+nbolli00@users.noreply.github.com>
2017-12-05 17:48:17 -0500

    Update morsegui.java

--- morsegui.java
+++ morsegui.java
@@ -1,3 +1,4 @@
+//The GUI of the Morse translator program
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
@@ -11,31 +12,32 @@ public class morsegui extends JFrame{
 	MorseListener ml = new MorseListener();
 	int but_x_base = 40;
 	int but_y_pos = 40;
-	int but_width = 60;
-	int but_height = 40;
-	JTextField txtf = new JTextField();
+	int but_width = 120;
+	int but_height = 20;
+	JTextArea txtf = new JTextArea();
 	JButton wait = new JButton("Wait");
 	JButton dot = new JButton("dot");
 	JButton dash = new JButton("dash");
-	JButton startstop = new JButton("Start/Stop");
+	JButton startstop = new JButton("Start");
+	JButton clear = new JButton("Clear");
 
 	// override constructor
 	public morsegui(){
 		
 		// This defines the JFrame
 		JFrame f = new JFrame("Morse Code Translater");
-		f.setBackground (new Color(255, 255, 255));
-		f.setForeground (new Color(153, 204, 255));
+		f.getContentPane().setBackground (new Color(156, 170, 223));
 		f.setSize(1000,600);
 		f.setLayout(null);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		// defines objects on the JFrame
-		txtf.setBounds(40, 10, 240, 20);
-		wait.setBounds(but_x_base + 60,but_y_pos + 200,but_width,but_height);
-		dot.setBounds(but_x_base + 0,but_y_pos + 80,but_width,but_height);
-		dash.setBounds(but_x_base + 60,but_y_pos + 80,but_width,but_height);
-		startstop.setBounds(but_x_base + 120,but_y_pos + 80,but_width,but_height);
+		txtf.setBounds(40, 325, 300, 220);
+		wait.setBounds(but_x_base + 120,but_y_pos + 20,but_width,but_height);
+		dot.setBounds(but_x_base + 120,but_y_pos + 60,but_width,but_height);
+		dash.setBounds(but_x_base + 120,but_y_pos + 100,but_width,but_height);
+		startstop.setBounds(but_x_base + 120,but_y_pos + 140,but_width,but_height);
+		clear.setBounds(but_x_base + 120,but_y_pos + 180,but_width,but_height);
 		ButtonListener listener = new ButtonListener();
 
 		// adds listeners to the buttons
@@ -43,6 +45,7 @@ public class morsegui extends JFrame{
 		dot.addActionListener(listener);
 		dash.addActionListener(listener);
 		startstop.addActionListener(listener);
+		clear.addActionListener(listener);
 
 		// adds the objects to the JFrame
 		f.add(txtf);
@@ -50,14 +53,14 @@ public class morsegui extends JFrame{
 		f.add(dot);
 		f.add(dash);
 		f.add(startstop);
+		f.add(clear);
 		
 		//setting the color of the buttons
 		wait.setBackground(new Color(128, 128, 128));
 		dot.setBackground(new Color(128, 128, 128));
 		dash.setBackground(new Color(128, 128, 128));
-		startstop.setBackground(new Color(128, 128, 128));
-		
-		//setting the color of the background
+		startstop.setBackground(Color.GREEN);
+		clear.setBackground(new Color(128, 128, 128));
 		
 		// makes it visible
 		f.setVisible(true);  
@@ -73,10 +76,13 @@ public class morsegui extends JFrame{
 		public void actionPerformed(ActionEvent e) {
 			if (e.getSource() == startstop && (!(ml.getListening()))) {
 				ml.listen();
-				System.out.println("listening!");
+				startstop.setBackground(Color.RED);
+				startstop.setText("Stop");
 			}
 			else if (e.getSource() == startstop && ml.getListening())  {
-				System.out.println(ml.stopListen());
+				txtf.append(ml.stopListen());
+				startstop.setBackground(Color.GREEN);
+				startstop.setText("Start");
 			}
 			else if (e.getSource() == wait) 
 				ml.getInput(0);
@@ -84,6 +90,8 @@ public class morsegui extends JFrame{
 				ml.getInput(1);
 			else if (e.getSource() == dash) 
 				ml.getInput(2);
+			else if (e.getSource() == clear) 
+				txtf.setText("");
 		}
 	}
 }
